c6c54ce2302c502cc4d5a3f8f946cb22b22e0384
Koen Kamman <koen@kamman.dev>
2023-08-02 18:02:24 +0200

    Fix sitemap url

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'http://localhost:3000',
+  siteUrl: 'https://koen.kamman.dev',
   generateRobotsTxt: true,
   generateIndexSitemap: false
 }
